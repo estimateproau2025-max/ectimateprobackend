399512d6b697ad39d30285d30e4c488118728dca
EhteshamButt <ehteshambutt58@gmail.com>
2025-12-14 00:00:27 -0800

    a

--- src/config/index.js
+++ src/config/index.js
@@ -7,15 +7,17 @@ const config = {
   env: process.env.NODE_ENV || "development",
   port: process.env.PORT || 5000,
   databaseUrl:
-    process.env.DATABASE_URL || "mongodb+srv://ehteshambutt58:4G9PFxLyIR6PqGLn@cluster0.mw68zmh.mongodb.net/number_discussion?retryWrites=true&w=majority",
+    // process.env.DATABASE_URL || "mongodb+srv://ehteshambutt58:4G9PFxLyIR6PqGLn@cluster0.mw68zmh.mongodb.net/number_discussion?retryWrites=true&w=majority",
+    process.env.DATABASE_URL || "mongodb+srv://estimateproau2025_db_user:estimatepro@1122@cluster0.mw68zmh.mongodb.net/number_discussion?retryWrites=true&w=majority",
+
   frontendUrl:
     process.env.FRONTEND_URL ||
     (process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}`
-      : "https://estimate-pro-chi.vercel.app"),
+      : "https://ectimatepros.vercel.app"),
   allowedOrigins: (
     process.env.CORS_ALLOW_ORIGINS ||
-    "http://localhost:5173,https://estimate-pro-chi.vercel.app"
+    "http://localhost:5173,https://ectimatepros.vercel.app"
   )
     .split(",")
     .map((origin) => origin.trim())
